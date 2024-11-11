b04b248f4dad5124bbd5246697112e7ad594b689
usama387 <128306049+usama387@users.noreply.github.com>
2024-11-11 11:28:00 +0500

    Configured cloudinary in cloudinary,js and imported it in server.js to use

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectCloudinary from "./config/cloudinary.js";
+
 
 // app config
 const app = express();
 const port = process.env.PORT || 4000;
-
+connectCloudinary();
 
 // middlewares
 app.use(express.json());
 app.use(cors())
+
 
 // api end points
 app.get('/', (req, res) => {
@@ -20,3 +23,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`The server is running on ${port}`)
 })
+
