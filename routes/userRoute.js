91ec625db7d5756d4c8d35d6fcac8ead5d20320a
usama387 <128306049+usama387@users.noreply.github.com>
2024-11-17 10:35:01 +0500

    Added a new post route connected with two middlewares and  updateProfile controller to update profile

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,13 +3,16 @@ import {
   getProfile,
   loginUser,
   registerUser,
+  updateProfile,
 } from "../controllers/userController.js";
 import authUser from "../middlewares/authUser.js";
+import upload from "../middlewares/multer.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
-userRouter.get("/get-profile",authUser, getProfile);
+userRouter.get("/get-profile", authUser, getProfile);
+userRouter.post("/update-profile",upload.single("image"),authUser, updateProfile);
 
 export default userRouter;
