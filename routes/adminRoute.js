5514cb53f9f8f4d260c9d7477d9761354849a2d7
usama387 <128306049+usama387@users.noreply.github.com>
2024-11-21 20:58:49 +0500

    Two new routes added with get and Post method to get all appointments for admin and cancel an appointment as an admin

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { addDoctor, adminLogin, getAllDoctors } from "../controllers/adminController.js";
+import {
+  addDoctor,
+  adminLogin,
+  cancelAppointmentAsAdmin,
+  getAllAppointmentsAdmin,
+  getAllDoctors,
+} from "../controllers/adminController.js";
 import upload from "../middlewares/multer.js";
 import authAdmin from "../middlewares/authAdmin.js";
 import { changeAvailability } from "../controllers/doctorController.js";
@@ -9,7 +15,9 @@ const adminRouter = express.Router();
 
 adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctor);
 adminRouter.post("/admin-login", adminLogin);
-adminRouter.post("/all-doctors",authAdmin, getAllDoctors);
-adminRouter.post("/change-availability",authAdmin, changeAvailability);
+adminRouter.post("/all-doctors", authAdmin, getAllDoctors);
+adminRouter.post("/change-availability", authAdmin, changeAvailability);
+adminRouter.get("/appointments", authAdmin, getAllAppointmentsAdmin);
+adminRouter.post("/cancel-appointment", authAdmin, cancelAppointmentAsAdmin);
 
 export default adminRouter;
